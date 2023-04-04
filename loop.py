3878138dd7df11e6fa0b03c2816dafc494e0b547
Michiel <44088477+Knitser@users.noreply.github.com>
2023-04-04 12:21:23 +0200

    added custom commands

--- loop.py
+++ loop.py
@@ -2,11 +2,11 @@ import itertools
 import serial
 import time
 
-ser = serial.Serial('COM5', 115200)
+ser = serial.Serial('COM3', 115200)
 
-with open('can.csv', 'r') as file:
+with open('can2.csv', 'r') as file:
     lines = itertools.cycle(file)
     for line in lines:
         ser.write(line.encode())
         print(ser.readline().decode('utf-8').strip())
-        time.sleep(0.1)
+        time.sleep(0.050)
